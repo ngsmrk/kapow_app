4caf74104ef7390c5a996131a30642823c2fdd70
ngsmrk <ngsmrk@gmail.com>
2012-05-20 19:22:55 +0100

    v1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kapow::Application.config.secret_token = '626ee58026cb7501d56577fa669fdc9f6948acfca5b53d5cda86c5ba1eb2e9ce1ad8bc697563a75c416a751f53f1aabf5f170097e2ff26b45acd65414b67274e'
