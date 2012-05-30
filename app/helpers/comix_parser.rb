7a111a1df9181912eb08f97710afbd1a39afdb19
ngsmrk <ngsmrk@gmail.com>
2012-05-30 18:27:53 +0100

    Now finds subsections

--- app/helpers/comix_parser.rb
+++ app/helpers/comix_parser.rb
@@ -1,18 +1,24 @@
 class ComixParser
 
-  attr_accessor :shipping_date
+  SUBSECTIONS = ['DARK HORSE COMICS', 'DC COMICS','IDW PUBLISHING','IMAGE COMICS','MARVEL COMICS','COMICS','MAGAZINES','MERCHANDISE']
+
+  attr_reader :shipping_date, :subsections
 
   def initialize(data)
     @data = data
+    @subsections = []
   end  
 
   def parse
 
     @data.each_with_index { | value, index |
       puts "Line #{index}: #{value}"
-    }
-        
+      
+      @subsections << value if SUBSECTIONS.include?value
+    }   
+            
     @shipping_date = DateTime.strptime(@data[2].split[1], '%m/%d/%Y')
+    
   end  
 
 end
--- spec/helpers/comix_parser_spec.rb
+++ spec/helpers/comix_parser_spec.rb
@@ -3,13 +3,21 @@ describe 'ComixParser' do
   context 'when it processes new releases' do
 
     before :each do
-      new_releases = ComixScraper.new('http://www.previewsworld.com/').get_new_releases    
+      
+      f = File.open('spec/samples/new_releases.txt', 'r')
+      new_releases = f.readline.split('\r\n')
+      
+      # new_releases = ComixScraper.new('http://www.previewsworld.com/').get_new_releases    
       @parser = ComixParser.new(new_releases)
       @parser.parse   
     end
 
     it 'it can find the shipping date' do
       @parser.shipping_date.to_date.should == DateTime.now.to_date   
+    end
+    
+    it 'it can find the subsection handings' do
+      @parser.subsections.should   =~ ["COMICS", "DARK HORSE COMICS", "DC COMICS", "IDW PUBLISHING", "IMAGE COMICS", "MAGAZINES", "MARVEL COMICS", "MERCHANDISE"]            
     end
  
   end
