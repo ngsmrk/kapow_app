aeef0ccbae42592165f0543d096b4dcadbb1f969
ngsmrk <ngsmrk@gmail.com>
2012-06-12 17:52:52 +0100

    Now displays releases

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -3,11 +3,19 @@ class ReleasesController < ApplicationController
   before_filter :init_scraper
   
   def new
-    @releases = @scraper.get_new_releases  
+    data = @scraper.get_new_releases
+    @parser = Kapow::ComixParser.new(data) 
+    @parser.parse     
+    @releases = @parser.comix
+    @shipping_date = @parser.shipping_date
   end
 
   def upcoming
-    @releases = @scraper.get_upcoming_releases
+    data = @scraper.get_upcoming_releases
+    @parser = Kapow::ComixParser.new(data) 
+    @parser.parse     
+    @releases = @parser.comix
+    @shipping_date = @parser.shipping_date    
   end
   
   private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
-module ApplicationHelper
+module ApplicationHelper  
+  def format_date_time(date)
+    date = Date.parse(date) unless date.respond_to?('strftime')
+    date != nil ? date.strftime("%d/%b/%Y") : ""
+  end  
 end
